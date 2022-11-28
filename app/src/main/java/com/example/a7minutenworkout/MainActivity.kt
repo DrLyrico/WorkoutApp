7a561a1c3fc3b9f128a3345030ce8f21db424e43
DrLyrico <michael.geyerlechner@gmail.com>
2022-11-28 18:30:21 +0100

    Update

--- app/src/main/java/com/example/a7minutenworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutenworkout/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.a7minutenworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        llStart.setOnClickListener {
+            Toast.makeText(this, "Hier beginnen die aufgaben", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
